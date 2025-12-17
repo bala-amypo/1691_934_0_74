8e66f3d077b6fdf2936d9ab3f684530fc942295b
bala-amypo <bala@amypo.in>
2025-12-17 08:36:24 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
@@ -6,3 +6,5 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepo;
 import com.example.demo.service.StudentService;
+
+@Overwrite
